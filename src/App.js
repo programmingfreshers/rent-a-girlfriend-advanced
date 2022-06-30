98ca3144eda47b9f141f893a5109480e65821b56
programmingfreshers <77062180+programmingfreshers@users.noreply.github.com>
2022-06-30 16:24:05 +0530

    title change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  document.title = 'rent a girlfriend';
   return (
     <>
-     <h3>hello </h3> 
+      
     </>
   );
 }
